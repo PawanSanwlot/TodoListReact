e7d33ff3806d7622edfd4247b9b61c3db85cc82e
Ryker <82869753+PawanSanwlot@users.noreply.github.com>
2024-05-13 13:10:24 +0530

    basic functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  const [todoData, setTodoData] = useState({
+    id: "",
+    todo: "",
+    isFinished: false
+  })
+
+ function addTodo() {
+  if (todoData.todo !== "") {
+      setTodos(prevTodos => {
+      return [...prevTodos, todoData]
+    })
+    setTodoData({...todoData, todo: ""})
+  }
+ }
+
+ function deleteTodo(id) {
+  setTodos((prevTodos) => {
+    return prevTodos.filter(item => item.id !== id)
+  })
+ }
+
+ function toggleIsFinished(id) {
+  setTodos((prevTodos => {
+    return prevTodos.map(item => {
+      if (item.id === id) {
+        return {...item, isFinished: !item.isFinished}
+      }
+      return item
+    })
+  }))
+ }
+
+  const todosElements = todos.map((item) => {
+     return <li key={item.id} className='list-group-item' style={{display: 'flex', justifyContent: "space-between"}}>
+        <span style={{cursor: "pointer", textDecoration: item.isFinished ? "line-through" : "none"}}
+          onClick={() => toggleIsFinished(item.id)}
+        >
+          {item.todo}</span>
+        <span><button onClick={() => deleteTodo(item.id)} className='btn btn-outline-danger btn-sm'>Delete</button></span>
+      </li>
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='app-container container'>
+      <h1>My Todo List</h1>
+      <input value={todoData.todo} type="text" placeholder='todo' className='form-control todoInput' onChange={(e) => setTodoData({id: uuidv4(), todo: e.target.value, isFinished: false})}/>
+      <button onClick={addTodo} className='btn btn-success btn-sm' style={{marginTop: "10px"}}>Enter</button>
+      <ul className='list-group' style={{marginTop: "20px"}}>
+       {todosElements}
+      </ul>
+    </div>
   )
 }
 
